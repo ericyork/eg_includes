5830483c33195e3a6a77eca732a5474b2dd3b9e9
ericyork <eyork@clarkson.edu>
2020-02-15 12:51:55 -0500

    Adds support for dynamic page titles

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<?php $pslug = "Home" ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>eg_includes</title>
+    <title>PHP Intro | <?php echo isset($pslug) ? strip_tags($pslug) : "Page not found" ?></title>
   </head>
   <body>
+    <nav>
+    </nav>
     <h1>Hello, Includes!</h1>
   </body>
 </html>
